3961f05d46551cd317ef97a02598149a6246d8f4
kaushik samanta <kaushiksamanta23@gmail.com>
2017-11-22 23:41:27 -0500

    twilio changes

--- Config/smsConfig.js
+++ Config/smsConfig.js
@@ -1,8 +1,8 @@
 'use strict';
 var twilioCredentials = {
-    accountSid : '',
-    authToken : '',
-    smsFromNumber : ''
+    accountSid : 'ACd61d3edd5c28e57b16a7d3',
+    authToken : '2275a70b2c66213c6f411389c82d0ab8',
+    smsFromNumber : '+14154'
 };
 
 module.exports = {
